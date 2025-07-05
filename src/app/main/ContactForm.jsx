3743a5bad725b62ac893cebb7ca4bb988952814b
Jenas Anton <jenasav010@gmail.com>
2025-07-05 19:48:49 +0530

    Contact changes

--- src/app/main/ContactForm.jsx
+++ src/app/main/ContactForm.jsx
@@ -16,55 +16,55 @@ const EmailSection = () => {
   }
 
   return (
-    <section id="contact" className="py-24 px-4 max-w-2xl mx-auto">
-      <h5 className="text-2xl font-bold text-white mb-6">Let&apos;s Connect</h5>
+    <section id="contact" className="py-28 px-6 max-w-2xl mx-auto">
+      <h1 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500 mb-6">Let&apos;s Connect</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-6">
-        <div className="flex flex-col md:flex-row gap-4">
+        <div className="flex flex-col md:flex-row gap-10">
           <div className="flex-1">
-            <label htmlFor="firstName" className="text-white text-sm block mb-2">First Name</label>
+            <label htmlFor="firstName" className="text-white text-xl block mb-2">First Name</label>
             <input
               id="firstName"
               type="text"
               name="firstName"
               required
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg w-full p-2.5"
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg w-full p-4"
               placeholder="John"
             />
           </div>
 
           <div className="flex-1">
-            <label htmlFor="lastName" className="text-white text-sm block mb-2">Last Name</label>
+            <label htmlFor="lastName" className="text-white text-xl block mb-2">Last Name</label>
             <input
               id="lastName"
               type="text"
               name="lastName"
               required
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg w-full p-2.5"
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg w-full p-4"
               placeholder="Doe"
             />
           </div>
         </div>
 
         <div>
-          <label htmlFor="email" className="text-white text-sm block mb-2">Your Email</label>
+          <label htmlFor="email" className="text-white text-xl block mb-2">Your Email</label>
           <input
             id="email"
             type="email"
             name="email"
             required
-            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg w-full p-2.5"
+            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg w-full p-4"
             placeholder="youremail@company.com"
           />
           <ValidationError prefix="Email" field="email" errors={state.errors} />
         </div>
 
         <div>
-          <label htmlFor="message" className="text-white text-sm block mb-2">Message</label>
+          <label htmlFor="message" className="text-white text-xl block mb-2">Message</label>
           <textarea
             id="message"
             name="message"
             required
-            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg w-full p-2.5"
+            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg w-full p-4"
             placeholder="Let's talk about..."
           />
           <ValidationError prefix="Message" field="message" errors={state.errors} />
@@ -73,7 +73,7 @@ const EmailSection = () => {
         <button
           type="submit"
           disabled={state.submitting}
-          className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg"
+          className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-100 px-10 rounded-lg"
         >
           Send Message
         </button>
